ce88f2ba54f1b26993b4bc95f486236bb0a8ff14
Richard Wilson <richard@freerunningtech.com>
2014-07-31 14:41:44 -0700

    Only use the taxons title if its a thing

--- app/controllers/spree/taxons_controller_decorator.rb
+++ app/controllers/spree/taxons_controller_decorator.rb
@@ -2,10 +2,6 @@ Spree::TaxonsController.class_eval do
   private
 
   def title
-    if @taxon
-      @taxon.seo_title
-    else
-      super
-    end
+    @taxon.try!(:seo_title).present? ? @taxon.seo_title : super
   end
 end
