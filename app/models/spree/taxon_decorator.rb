1a713553b07178dcc36b8bea938f538f186f2d29
Richard Wilson <richard@freerunningtech.com>
2014-07-31 09:35:23 -0700

    Fix title settings, and meta keywords.

--- app/models/spree/taxon_decorator.rb
+++ app/models/spree/taxon_decorator.rb
@@ -1,10 +1,4 @@
 Spree::PermittedAttributes.taxon_attributes << :header
-
 Spree::Taxon.class_eval do
-
-  has_attached_file :header,
-    styles: { normal: '1000x' },
-    default_style: :normal,
-    default_url: '/assets/default_taxon.png'
-
+  has_attached_file :header
 end
